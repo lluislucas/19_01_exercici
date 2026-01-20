b17ee05aa7736367dce6bae7b2b11a926329bf08
LLuis Lucas <llucas@cendrassos.net>
2026-01-20 19:11:15 +0100

    he modificat el bool pq digui si/no. Metdoe MostrarInformacio no funciona, nose com ficar un metode dins un metode?. Mira comentaris dins el codi

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,74 @@
-﻿namespace ConsoleApp1;
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp1;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Digues el nom del producte");
+        string nom = Console.ReadLine();
+        
+        Console.WriteLine("Digues el preu del producte");
+        double preu = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("Digues el descompte del producte");
+        double descompte = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine(CalcularPreuAmbIVA(preu));
+        
+        Console.WriteLine(CalcularPreuAmbDescompte(descompte, preu));
+        
+        Console.WriteLine(EsPreuSuperiorACinquanta(preu));
+
+       MostrarInformacio(nom, preu, descompte); //no funciona
+
+    }
+
+    static double CalcularPreuAmbIVA(double preu)
+    {
+        return preu * 1.21;
+        
+        /* double preu_ambIVA = preu 1.21
+         return preu_ambIVA;*/
+    }
+
+    static double CalcularPreuAmbDescompte(double descompte, double preu)
+    {
+        return preu * (1 - (descompte / 100)); 
+    }
+
+    static string EsPreuSuperiorACinquanta(double preu) // he modificat pq contesti si o no
+    {
+
+        //return preu > 50;
+
+        bool es_superior = false;
+        string resposta;
+        
+        if (preu > 50)
+        {
+            es_superior = true;
+             resposta = "si";
+        }
+        else
+        {
+             resposta = "no";
+        }
+
+        return resposta;
+    }
+    
+
+    static void MostrarInformacio(string nom, double preu, double descompte) //no funciona
+    {
+        Console.WriteLine("----Informació producte----");
+        Console.WriteLine($"Nom: {nom}");
+        Console.WriteLine($"Preu base: {preu}€");
+        Console.WriteLine($"Preu amb IVA: {CalcularPreuAmbIVA(preu)}€");
+        Console.WriteLine($"Preu amb descompte: {CalcularPreuAmbDescompte(descompte, preu)}€");
+        Console.WriteLine($"El preu final del producte és superior a 50€: {EsPreuSuperiorACinquanta(preu)}");
+        
     }
 }
+
--- ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net10.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+436540f3acbe80290b7550bdee5d15e0a64534bc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
